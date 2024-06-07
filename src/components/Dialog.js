2478e040aa9a94c78af1e77c184422bf39b07c63
nnifemi <odeyeminifemi@icloud.com>
2024-06-07 12:45:10 -0500

    Dialog.js added

---
+++ src/components/Dialog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dialog = ({ closeDialog }) => {
+    return (
+        <div className="dialog center-screen dialog-img"></div>
+    );
+};
+
+export default Dialog;
